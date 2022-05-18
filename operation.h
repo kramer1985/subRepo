7a4d9f6169858cce8afdbd34a621fdad68a7a60c
yassine necib <necib.yassine.tux@gmail.com>
2022-05-18 00:09:52 -0300

    Added substraction feature

--- operation.h
+++ operation.h
@@ -3,3 +3,4 @@
 #endif
 unsigned int addition (const unsigned a, const unsigned b);
 unsigned int mult (const unsigned a, const unsigned b);
+signed int sub (const signed a, const signed b);
---
+++ sub.c
@@ -0,0 +1,7 @@
+#include "operation.h"
+
+
+signed int sub(const signed a, const signed b)
+{
+	return a-b;
+}
