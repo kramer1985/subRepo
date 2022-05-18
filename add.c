db809cd67e0f887b9f639ece861532cdce2afb4f
yassine necib <necib.yassine.tux@gmail.com>
2022-05-17 23:53:38 -0300

    Added additions feature

---
+++ add.c
@@ -0,0 +1,7 @@
+#include "operation.h"
+
+
+unsigned int addition(const unsigned a, const unsigned b)
+{
+	return a+b;
+}
---
+++ operation.h
@@ -0,0 +1,4 @@
+#ifndef operation_h
+#define operation_h
+#endif
+unsigned int addition (const unsigned a, const unsigned b);
