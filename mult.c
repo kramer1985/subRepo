b6282de5c5b43cd655f3f26ccc4da2a2ee9ac733
yassine necib <necib.yassine.tux@gmail.com>
2022-05-17 23:58:44 -0300

    Added multiplication feature

---
+++ mult.c
@@ -0,0 +1,7 @@
+#include "operation.h"
+
+
+unsigned int mult(const unsigned a, const unsigned b)
+{
+	return a*b;
+}
--- operation.h
+++ operation.h
@@ -2,3 +2,4 @@
 #define operation_h
 #endif
 unsigned int addition (const unsigned a, const unsigned b);
+unsigned int mult (const unsigned a, const unsigned b);
